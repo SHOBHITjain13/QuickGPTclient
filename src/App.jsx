16fc45022099c565e20270f8f9c68fcba2dbd07f
SHOBHITjain13 <shobbitjain13@gmail.com>
2025-09-30 13:39:09 +0530

    UI is done

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,11 @@ import { assets } from './assets/assets'
 import './assets/prism.css'
 import Loading from './pages/Loading'
 import Login from './pages/Login'
+import { useAppContext } from './context/AppContext'
 
 const App = () => {
+
+  const {user} = useAppContext()
 
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const {pathname} = useLocation()
@@ -19,7 +22,9 @@ const App = () => {
     <>
       {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3
       w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={()=>setIsMenuOpen(true)}/>}
-      <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+      
+      {user ? (
+        <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
         <div className='flex h-screen w-screen'>
           <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
           <Routes>
@@ -32,6 +37,14 @@ const App = () => {
         </div>
       </div>
 
+      ) : (
+        <div className='bg-gradient-to-b from-[#242124] to-[#000000] flex
+        items-center justify-center h-screen w-screen'>
+          <Login/>
+        </div>
+      )}
+      
+      
     </>
   )
 }
