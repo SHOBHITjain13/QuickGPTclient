8b6044e26bcbfaecd359c37a4a05b9fd805be277
SHOBHITjain13 <shobbitjain13@gmail.com>
2025-10-03 22:38:57 +0530

    frontend complete

--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -35,7 +35,7 @@ const Credits = () => {
                   <h3 className='text-xl font-semibold text-gray-900 dark:text-white
                   mb-2'>{plan.name}</h3>
                   <p className='text-2xl font-bold text-purple-600 dark:text-purple-300
-                  mb-4'>₹{plan.price}
+                  mb-4'>${plan.price}
                     <span className='text-base font-normal text-gray-600
                     dark:text-purple-200'>{' '}/ {plan.credits}</span>
                   </p>
